f4a0107735a59c04889f65169c680190e2b22545
harmys78 <harishs.srinivasan@gmail.com>
2021-10-08 02:25:54 +0530

    updated auth client

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -25,7 +25,7 @@ export default () => {
     <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
-        <label>Email Address</label>
+        <label>Email Address123</label>
         <input
           value={email}
           onChange={e => setEmail(e.target.value)}
